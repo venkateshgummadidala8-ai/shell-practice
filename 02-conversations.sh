851f88335a9506c700300b36c741a7e7744d858a
venkateshg8 <venkatesh.gummadidala8@gmail.com>
2026-05-21 21:58:25 +0530

    hello word

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -3,6 +3,6 @@
 PERSON1="RUSSIA"
 PERSON2="UKRAINE"
 
-echo  "${PERSON1}: hey ${PERSON2} what are you going to do with your nuclear program?"
-echo "${PERSON2}: we are going to use it for peaceful purposes"
-echo "${PERSON1}: you are lying, we know you are going to use it for bad purposes"
+echo  "$PERSON1: hey $PERSON2 what are you going to do with your nuclear program?"
+echo "$PERSON2: we are going to use it for peaceful purposes"
+echo "$PERSON1: you are lying, we know you are going to use it for bad purposes"
