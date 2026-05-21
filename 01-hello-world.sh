bc703fc35f230b441bea8f5ecd7b3962f22e88e6
venkateshg8 <venkatesh.gummadidala8@gmail.com>
2026-05-21 21:25:21 +0530

    hello world

---
+++ 01-hello-world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+# this is comment, it will not be executed
+echo "Hello World"
+echo "welcome to shell scripting"
+echo "this is my first shell scripting"
+echo "i am learning shell scripting"
+
