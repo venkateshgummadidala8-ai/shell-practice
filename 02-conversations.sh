9d43470a6b6aa6d433a5fa1c25b94aae78608282
venkateshg8 <venkatesh.gummadidala8@gmail.com>
2026-05-21 21:43:46 +0530

    hello word

---
+++ 02-conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo  "Trump: hey Iran what are you going to do with your nuclear program?"
+echo "iran: we are going to use it for peaceful purposes"
+echo "Trump: you are lying, we know you are going to use it for bad purposes
+
