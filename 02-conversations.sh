76a6b06651c92043bf3c61b612e2b7deef9fa946
venkateshg8 <venkatesh.gummadidala8@gmail.com>
2026-05-21 21:49:09 +0530

    hello word

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo  "Trump: hey Iran what are you going to do with your nuclear program?"
-echo "iran: we are going to use it for peaceful purposes"
-echo "Trump: you are lying, we know you are going to use it for bad purposes
+PERSON1="RUSSIA"
+PERSON2="UKRAINE"
 
+echo  "${PERSON1}: hey ${PERSON2} what are you going to do with your nuclear program?"
+echo "${PERSON2}: we are going to use it for peaceful purposes"
+echo "${PERSON1}: you are lying, we know you are going to use it for bad purposes"
