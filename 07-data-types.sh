7e58554f762f2cabfb064313cd18c88d38d2366c
venkateshg8 <venkatesh.gummadidala8@gmail.com>
2026-05-24 00:45:39 +0530

    hello world

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -5,7 +5,7 @@ SUM=$($NUM1+$NUM2)
 echo "sum is $SUM"
 ## Array
 MOVIES =("RRR", "KGF", "Bahubali") # index is RRR always 0, KGF is 1 and Bahubali is 2
-echo "movies are: ${MOVIES[@]}
-echo "first movie is : ${MMOVIE[0]}"
+echo"movies are: ${MOVIES[@]}
+echo"first movie is : ${MMOVIE[0]}"
 echo "second movie is : ${MOVIE[1]}"
 
