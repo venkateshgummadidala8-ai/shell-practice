699d40558690cf104c0fa9d4fdef4958d3865d1f
venkateshg8 <venkatesh.gummadidala8@gmail.com>
2026-05-26 22:54:48 +0530

    hello world

--- 09-install.sh
+++ 09-install.sh
@@ -8,6 +8,17 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi   
 
+# first arg -> what are you trying to install
+# second arg -> Exit code
+VALIDATE() {
+    if [ $2 -ne 0 ]; then 
+        echo " Installing $1 is failed "
+        exit 1
+    else 
+        echo "$1 installed succesfully"
+    fi
+}
+
 # echo " i am continuing ....."
 dnf list install mysql
 
@@ -16,15 +27,20 @@ if [ $? -ne 0 ]; then
 else
     echo " installing mysql "
     dnf install mysql -y
-
-
-    if [ $? -ne 0 ]; then 
-        echo " Installing mysql is failed "
-        exit 1
-    else 
-        echo "mysql istalled succesfully"
-    fi
+    VALIDATE "mysql" $?
+  
 fi    
 
 
+# install nginx
+dnf list installed ngnix
+if [ $? -ne 0 ]; then
+    echo " nginx is already installed...skipping "
+    else 
+        echo " installing nginx "
+        dnf install nginx -y
+        VALIDATE "nginx" $?
+fi
+ 
+ 
 
