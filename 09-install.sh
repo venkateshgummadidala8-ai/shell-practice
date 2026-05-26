3784db7dfd911e36b555c1c639a1abc43d59b0a0
venkateshg8 <venkatesh.gummadidala8@gmail.com>
2026-05-26 16:28:12 +0530

    hello world

--- 09-install.sh
+++ 09-install.sh
@@ -8,4 +8,16 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi   
 
-echo " i am continuing ....."
+# echo " i am continuing ....."
+
+echo " installing MySQL "
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then 
+    echo " Installing mysql is failed "
+    ecit 1
+else 
+    echo "mysql istalled succesfully"
+fi
+
+
