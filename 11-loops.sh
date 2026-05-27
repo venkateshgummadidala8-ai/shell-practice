41d34a42ae388568d72e903cf94f8f14538c0a78
venkateshg8 <venkatesh.gummadidala8@gmail.com>
2026-05-27 17:32:31 +0530

    hello world

--- 11-loops.sh
+++ 11-loops.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-for i in {1..100}
-do
-    echo $i
+USERID=$(id -u)
+
+# check root access or not
+if [ $USERID -ne 0 ]; then
+    echo "please run the script with root access"
+    exit 1
+fi   
+
+# first arg -> what are you trying to install
+# second arg -> Exit code
+VALIDATE() {
+    if [ $2 -ne 0 ]; then 
+        echo " Installing $1 is failed "
+        exit 1
+    else 
+        echo "$1 installed succesfully"
+    fi
+}
+
+for package in $@
+do 
+    echo " installing $package "
+
+
 done
