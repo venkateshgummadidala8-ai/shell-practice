275304d59552efaa71ac25579f49e0dcab6a21f8
venkateshg8 <venkatesh.gummadidala8@gmail.com>
2026-05-30 10:29:57 +0530

    api doc added

---
+++ 12-loops.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S" )
+
+
+# check root access or not
+if [ $USERID -ne 0 ]; then
+    echo "please run the script with root access"
+    exit 1
+fi   
+
+# first arg -> what are you trying to install
+# second arg -> Exit code
+VALIDATE() {
+    if [ $2 -ne 0 ]; then 
+        echo " $TIMESTAMP [ERROR] Installing $1 is failed " | tee -a $LOGS_FILE
+        exit 1
+    else 
+        echo " $TIMESTAMP [INFO] $1 installed succesfully" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ #$@ will give all the arguments passed to the script
+do 
+    echo " installing $package "
+    dnf list installed $package &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
+        dnf install $package -y &>> $LOGS_FILE
+        VALIDATE "Installing $package" $?
+    else
+        echo "$package is already installed ....skipping"
+    fi
+done
